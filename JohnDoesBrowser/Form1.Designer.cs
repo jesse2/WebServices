8685a6c4f2a66c698a2853ad6b5768189cdb78c1
Jesse <jesse9726@gmail.com>
2017-05-22 04:32:29 -0700

    added services
    encryption and stocks

--- JohnDoesBrowser/Form1.Designer.cs
+++ JohnDoesBrowser/Form1.Designer.cs
@@ -31,6 +31,14 @@
             this.txtUrl = new System.Windows.Forms.TextBox();
             this.btnGo = new System.Windows.Forms.Button();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtEncrypt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtDecrypt = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtUrl
@@ -56,14 +64,91 @@
             this.webBrowser1.Location = new System.Drawing.Point(3, 29);
             this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
             this.webBrowser1.Name = "webBrowser1";
-            this.webBrowser1.Size = new System.Drawing.Size(700, 568);
+            this.webBrowser1.Size = new System.Drawing.Size(700, 500);
             this.webBrowser1.TabIndex = 2;
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.Location = new System.Drawing.Point(22, 536);
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.Size = new System.Drawing.Size(214, 20);
+            this.txtEncrypt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 536);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(338, 540);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(338, 566);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "label2";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(242, 562);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 20);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtDecrypt
+            // 
+            this.txtDecrypt.Location = new System.Drawing.Point(22, 562);
+            this.txtDecrypt.Name = "txtDecrypt";
+            this.txtDecrypt.Size = new System.Drawing.Size(214, 20);
+            this.txtDecrypt.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(453, 536);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(559, 536);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 20);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 602);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtDecrypt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtEncrypt);
             this.Controls.Add(this.webBrowser1);
             this.Controls.Add(this.btnGo);
             this.Controls.Add(this.txtUrl);
@@ -79,6 +164,14 @@
         private System.Windows.Forms.TextBox txtUrl;
         private System.Windows.Forms.Button btnGo;
         private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txtEncrypt;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtDecrypt;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- JohnDoesBrowser/Form1.cs
+++ JohnDoesBrowser/Form1.cs
@@ -21,5 +21,45 @@ namespace JohnDoesBrowser
         {
             webBrowser1.Navigate(txtUrl.Text);
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EncryptService.ServiceClient encrypt = new EncryptService.ServiceClient();
+            try
+            {
+                label1.Text = encrypt.Encrypt(txtEncrypt.Text);
+                txtDecrypt.Text= encrypt.Encrypt(txtEncrypt.Text);
+            }
+            catch(Exception ec)
+            {
+                label1.Text = ec.Message.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EncryptService.ServiceClient decrypt = new EncryptService.ServiceClient();
+            try
+            {
+                label2.Text = decrypt.Decrypt(txtDecrypt.Text);
+            }
+            catch (Exception ec)
+            {
+                label2.Text = ec.Message.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            stockService.ServiceClient quote = new stockService.ServiceClient();
+            try
+            {
+                String info = quote.getStockquote(textBox1.Text);
+                Console.WriteLine(info);
+            }catch(Exception sc)
+            {
+                Console.WriteLine(sc.Message.ToString());
+            }
+        }
     }
 }
